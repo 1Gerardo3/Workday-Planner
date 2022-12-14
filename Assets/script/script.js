505a6dce59ae96a951e6b393f8f1021485f4473f
gerardo Alvarez <1gerardo3@gmail.com>
2022-12-13 20:46:52 -0800

    add task to local storage

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,35 +1,33 @@
 $(document).ready(function () {
-    var hourTen = $("#10");
-    var hourEleven = $("#11");
-    var hourTwelve = $("#12");
-    var hourOne = $("#13");
-    var hourTwo = $("#14");
-    var hourThree = $("#15");
-    var hourFour = $("#16");
-    var hourFive = $("#17");
-    var hourSix = $("#18");
-    var hourSeven = $("#19");
-    var hourEight = $("#20");
-  
-  
-  
-    var currentHour = dayjs().format("H");
-    //var currentHour = 14;
-    console.log(currentHour);
-    var allTimes = [
-      hourTen,
-      hourEleven,
-      hourTwelve,
-      hourOne,
-      hourTwo,
-      hourThree,
-      hourFour,
-      hourFive,
-      hourSix,
-      hourSeven,
-      hourEight,
-    ];
-    //console.log(allTimes);
+  var hourTen = $("#10");
+  var hourEleven = $("#11");
+  var hourTwelve = $("#12");
+  var hourOne = $("#13");
+  var hourTwo = $("#14");
+  var hourThree = $("#15");
+  var hourFour = $("#16");
+  var hourFive = $("#17");
+  var hourSix = $("#18");
+  var hourSeven = $("#19");
+  var hourEight = $("#20");
+
+  var currentHour = dayjs().format("H");
+  //var currentHour = 14;
+  console.log(currentHour);
+  var allTimes = [
+    hourTen,
+    hourEleven,
+    hourTwelve,
+    hourOne,
+    hourTwo,
+    hourThree,
+    hourFour,
+    hourFive,
+    hourSix,
+    hourSeven,
+    hourEight,
+  ];
+  //console.log(allTimes);
   //Add code to display the current date and time in the header of the page.
   var currentDate = dayjs().format("dddd MMM D, YYYY, h:MM ");
   console.log(currentDate);
@@ -54,12 +52,23 @@ $(document).ready(function () {
     }
   }
 });
-// Need to add a onclick to save 
+// Need to add a onclick to save
 
 $(".btn").on("click", function () {
-    let task = $(this).siblings(".description").val();
-    console.log(task);
-    let time = $(this).parent().attr("id");
-    localStorage.setItem(time, task);
-  });
-  
+  let task = $(this).siblings(".description").val();
+  console.log(task);
+  let time = $(this).parent().attr("id");
+  localStorage.setItem(time, task);
+});
+
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+$("#18 .description").val(localStorage.getItem("18"));
+$("#19 .description").val(localStorage.getItem("19"));
+$("#20 .description").val(localStorage.getItem("20"));
